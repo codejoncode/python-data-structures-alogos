ec6ff02a4097bcd5edc0ca954cfef4995541e84a
codejoncode <jonathanjholloway@gmail.com>
2018-12-27 17:50:15 -0600

    some reformating of documents also made changes in the bubble sort to accept a comparision function that will perform the check if the items are greater or less than and return true or false. Have a working bubble sort of by_title_ascending using bubble sort because the books on the shelf or nearly sorted which allows bubble sort to be efficient.

--- sorting/A_Sorted_Tale/script.py
+++ sorting/A_Sorted_Tale/script.py
@@ -4,4 +4,17 @@ import sorts
 bookshelf = utils.load_books('C:/Users/Jonathan/LambdaSchoolProjects/python-data-structures-alogos/sorting/A_Sorted_Tale/books_small.csv')
 
 for book in bookshelf:
-    print(book['title'])
+    print(book['title_lower'])
+#end of for loop. 
+
+def by_title_ascending (book_a, book_b):
+    """
+    This function will be used to compare two books and returns 
+    true or false based off if book a is greater than book b 
+    comparision done on the books  title_lower key. 
+    """
+    return book_a['title_lower'] > book_b['title_lower']
+#end of by_title_ascending 
+
+sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
+print(sort_1)
--- sorting/A_Sorted_Tale/sorts.py
+++ sorting/A_Sorted_Tale/sorts.py
@@ -1,29 +1,38 @@
 import random
 
-def bubble_sort(arr):
-  swaps = 0
-  sorted = False
-  while not sorted:
-    sorted = True
-    for idx in range(len(arr) - 1):
-      if arr[idx] > arr[idx + 1]:
-        sorted = False
-        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
-        swaps += 1
-  print("Bubble sort: There were {0} swaps".format(swaps))
-  return arr
+
+def bubble_sort(arr, comparison_function):
+    """
+    bubble sort best used on data that is nearly sorted.  This bubble sort implemenation accepts a list and a function as paramaters. 
+    the function it uses will compare two elements and return true or false. 
+    """
+    swaps = 0
+    sorted = False
+    while not sorted:
+        sorted = True
+        for idx in range(len(arr) - 1):
+            #   if arr[idx] > arr[idx + 1]:
+            if comparison_function(arr[idx], arr[idx + 1]):
+                sorted = False
+                arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+                swaps += 1
+    print("Bubble sort: There were {0} swaps".format(swaps))
+    return arr
+# end of bubble sort
+
 
 def quicksort(list, start, end):
-  if start >= end:
-    return
-  pivot_idx = random.randrange(start, end + 1)
-  pivot_element = list[pivot_idx]
-  list[end], list[pivot_idx] = list[pivot_idx], list[end]
-  less_than_pointer = start
-  for i in range(start, end):
-    if pivot_element > list[i]:
-      list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
-      less_than_pointer += 1
-  list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
-  quicksort(list, start, less_than_pointer - 1)
-  quicksort(list, less_than_pointer + 1, end)
+    if start >= end:
+        return
+    pivot_idx = random.randrange(start, end + 1)
+    pivot_element = list[pivot_idx]
+    list[end], list[pivot_idx] = list[pivot_idx], list[end]
+    less_than_pointer = start
+    for i in range(start, end):
+        if pivot_element > list[i]:
+            list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+            less_than_pointer += 1
+    list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
+    quicksort(list, start, less_than_pointer - 1)
+    quicksort(list, less_than_pointer + 1, end)
+# end of quicksort
