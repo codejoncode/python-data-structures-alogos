35c666087b29e5dde9ed03b75b1b0a58e6639fd4
codejoncode <jonathanjholloway@gmail.com>
2019-01-16 22:12:05 -0600

    updated a word in the doc string

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,7 +1,7 @@
 """
 Bubble sort is a intro sorting algorithm that compares pairs of elements connected during iteration. 
 
-Swapping will occur to shit the elements to the begining or end of list. 
+Swapping will occur to shift the elements to the begining or end of list. 
 
 Two loops will be used to sort the list.   The first loop goes until the list is sorted or as long as the list is not sorted. 
 The assumption will be made from the start that the list is unsorted. The inner loop will perform our checks greater than or less than
